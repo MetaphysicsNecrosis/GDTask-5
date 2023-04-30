92ee3523bf3b817fd777301dfad25a68990423c5
MetaphysicsNecrosis <MetaphysicsNecrosis@DESKTOP-A9QI9P7>
2023-04-30 13:19:44 +0300

    Добавьте файлы проекта.

---
+++ GDTask-5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDTask_5
+{
+    class ToHashSetAndBack<template> 
+    {
+        static public List<template> ToHashSet(List<template> list) 
+        {
+            List<template> answer = new List<template>();
+            for (var itter = list.Count - 1; itter >= 0; itter--) 
+            {
+                if (answer.Contains(list[itter])) 
+                {
+                    continue;
+                }
+
+                answer.Add(list[itter]);
+            }
+            answer.Reverse();
+            return answer;
+        }
+    
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            arr = ToHashSetAndBack<int>.ToHashSet(arr);
+            for(int i = 0; i < arr.Count;i++) 
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
